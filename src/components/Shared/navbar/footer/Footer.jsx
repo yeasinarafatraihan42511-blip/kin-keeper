15486c481289925a88feb3a5f44e102ec67171fd
Yeasin <yeasinarafatraihan42511@gmail.com>
2026-04-16 15:39:12 +0600

    footer complete

--- src/components/Shared/navbar/footer/Footer.jsx
+++ src/components/Shared/navbar/footer/Footer.jsx
@@ -1,36 +1,55 @@
 import React from 'react';
+import { NavLink } from 'react-router';
+import instagram from '../../../../../public/instagram.png';
+import facebook from '../../../../../public/facebook.png';
+import twitter from '../../../../../public/twitter.png';
+
 
 
 
 
 const Footer = () => {
     return (
-        <footer className="bg-green-900 text-white mt-10 container mx-auto">
+        <footer className="bg-green-800 text-white mt-10 container mx-auto">
       <div className="max-w-7xl mx-auto px-6 py-10 text-center">
         {/* Brand */}
-        <h2 className="text-2xl font-bold">KeenKeeper</h2>
-        <p className="text-sm opacity-80 mt-2">
-          Keep your meaningful friendships alive with timely check-ins.
+        <h2 className="text-3xl font-bold">KeenKeeper</h2>
+        <p className="text-sm opacity-80 mt-2 ">
+          Your personal shelf of meaningful connections. Browse, tend, and nurture the
+relationships that matter most.
         </p>
 
-        {/* Social / Links (static for assignment) */}
-        <div className="flex justify-center gap-4 mt-4">
-          <span className="badge badge-outline">FB</span>
-          <span className="badge badge-outline">TW</span>
-          <span className="badge badge-outline">IG</span>
-        </div>
+        {/* Social Links  */}
+       <div>
+        <div>
+          <h2 className='text-bold text-2xl'>Social Links</h2>
 
-        {/* Bottom Links */}
+        </div>
+        <div className="flex justify-center gap-4 mt-4">
+            <span className=""><img src={instagram} alt="Instagram" /></span>
+            
+          <span className=""><img src={facebook} alt="Facebook" /></span>
+          <span className=""><img src={twitter} alt="Twitter" /></span>
+          
+        
+        </div>
+       </div>
+
+       <div className=' flex justify-between px-6'>
+         {/* Copyright */}
+        <p className="text-xs opacity-60 mt-6">
+          © 2026 KeenKeeper. All rights reserved.
+        </p>
+         {/* Bottom Links */}
         <div className="flex justify-center gap-6 mt-6 text-sm opacity-80">
+          
           <span>Privacy Policy</span>
           <span>Terms of Service</span>
           <span>Contact</span>
         </div>
 
-        {/* Copyright */}
-        <p className="text-xs opacity-60 mt-6">
-          © 2026 KeenKeeper. All rights reserved.
-        </p>
+       
+       </div>
       </div>
     </footer>
     );
